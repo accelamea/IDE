f98004e017f59c52ce5b0c8eeb328f8897b70c1c
SKO <SKO@192.168.168.39>
2016-08-04 16:31:40 +0400

    changing api

--- LIB/INCLUDE_RECORD.js
+++ LIB/INCLUDE_RECORD.js
@@ -1,5 +1,5 @@
 /**
- * this object is used to read any Accela records
+ * this object is used to read and modify any record type
  */
 /*-USER-----------DATE----------------COMMENTS----------------------------------------------------------/
  | SLEIMAN         06/01/2016 10:11:10 formatting code
