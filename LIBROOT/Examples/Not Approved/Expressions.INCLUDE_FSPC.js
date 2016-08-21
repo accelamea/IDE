f6f0020a947ca26b7e59412cc1b2182f3a5bd9e9
sleiman <skozaizan@accela.com>
2016-08-21 09:47:37 +0400

    this is a demo example

---
+++ LIBROOT/Examples/Not Approved/Expressions.INCLUDE_FSPC.js
@@ -0,0 +1,8 @@
+case FSPR.TYPES.Market:
+		sampleSource = "Market";
+		sampleReason = "Routine";
+		break;
+	case FSPR.TYPES.Survey:
+		sampleSource = "Market";
+		sampleReason = "Survey";
+		break;
