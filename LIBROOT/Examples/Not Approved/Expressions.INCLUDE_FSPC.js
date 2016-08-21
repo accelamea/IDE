2a7c7baa8529c49d1a6e0292141c7fa720e77619
SKO <SKO@skolap.router64e860.com>
2016-08-21 10:04:35 +0400

    2.0.3.5

--- LIBROOT/Examples/Not Approved/Expressions.INCLUDE_FSPC.js
+++
@@ -1,8 +0,0 @@
-case FSPR.TYPES.Market:
-		sampleSource = "Market";
-		sampleReason = "Routine";
-		break;
-	case FSPR.TYPES.Survey:
-		sampleSource = "Market";
-		sampleReason = "Survey";
-		break;
