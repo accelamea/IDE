ccf62feaab2f310fde1d4913ec6c42d3d3606619
SKO <SKO@192.168.168.53>
2016-08-21 11:13:10 +0400

    update ACABASE, BETA version not tested

--- LIBROOT/Libs/INCLUDE_ACABASE.js
+++ LIBROOT/Libs/INCLUDE_ACABASE.js
@@ -8,6 +8,9 @@
 | Created at	: 07/02/2016 11:26:47
 |
 /------------------------------------------------------------------------------------------------------*/
+/*-USER-----------DATE----------------COMMENTS----------------------------------------------------------/
+ | SALIM           25/05/2016 15:28:46   Adding new methods [setCapModel,setLicenseProfessional]
+ /-----END CHANGE LOG-----------------------------------------------------------------------------------*/
 
 SCRIPT_VERSION = "3.0";
 eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
@@ -25,12 +28,15 @@ function ACABASE() {
 	this.tdebug = "";
 	this.tmessage = "";
 	this.asiGroups = null;
+	this.asitGroups = null;
 	this.capModel = null;
 }
 
 /**
  * cancel the event and set the message
- * @param message the message to set
+ * 
+ * @param message
+ *            the message to set
  */
 ACABASE.prototype.cancel = function(message) {
 	this.bcancel = true;
@@ -45,14 +51,17 @@ ACABASE.prototype.showDebug = function() {
 
 /**
  * show message
- * @param message the message to show
+ * 
+ * @param message
+ *            the message to show
  */
 ACABASE.prototype.showMessage = function(message) {
 	this.bshowMessage = true;
 	this.tmessage = message;
 }
 /**
- * this is the main function for the execution, it should be defined in the page flow script
+ * this is the main function for the execution, it should be defined in the page
+ * flow script
  */
 ACABASE.prototype.execute = function() {
 	throw "Execute not implemented in base page"
@@ -67,6 +76,66 @@ ACABASE.prototype.getCapModel = function() {
 	}
 	return this.capModel;
 }
+/**
+ * @param CapModel :
+ *            the updated capModel sets the environment CapModel
+ */
+ACABASE.prototype.setCapModel = function(capModel) {
+	if (capModel == null) {
+		aa.env.setValue("CapModel", this.getCapModel());
+	} else {
+		aa.env.setValue("CapModel", capModel);
+	}
+}
+/**
+ * 
+ * @returns gets the ASI field value
+ */
+ACABASE.prototype.getAppSpecific = function(itemName) {
+	return this.getFieldValue(itemName);
+}
+/**
+ * 
+ * @returns sets the ASI field value
+ */
+ACABASE.prototype.editAppSpecific = function(fieldName, value) {
+	this.setFieldValue(fieldName, value);
+}
+/**
+ * 
+ * @param lp:
+ *            license Professional
+ * @result : license Professional attached to the CapModel
+ */
+ACABASE.prototype.setLicenseProfessional = function(lp) {
+	var thisCapModel = this.getCapModel();
+	thisCapModel.setLicenseProfessionalModel(lp);
+}
+
+/**
+ * 
+ * @returns gets the ASI field value from App Specific Info Groups
+ */
+ACABASE.prototype.getFieldValue = function(fieldName) {
+	var val = null;
+	var asiGroups = this.getAsiGroups();
+	var iteGroups = asiGroups.iterator();
+	while (iteGroups.hasNext()) {
+		var group = iteGroups.next();
+		var fields = group.getFields();
+		if (fields != null) {
+			var iteFields = fields.iterator();
+			while (iteFields.hasNext()) {
+				var field = iteFields.next();
+				if (fieldName == field.getCheckboxDesc()) {
+					val = field.getChecklistComment();
+					break;
+				}
+			}
+		}
+	}
+	return val;
+}
 
 /**
  * 
@@ -79,9 +148,22 @@ ACABASE.prototype.getAsiGroups = function() {
 	return this.asiGroups;
 }
 /**
+ * 
+ * @returns the ASIT groups of the CapModel of this page flow
+ */
+ACABASE.prototype.getAsitGroups = function() {
+	if (this.asitGroups == null) {
+		this.asitGroups = this.getCapModel().getAppSpecificTableGroupModel();
+	}
+	return this.asitGroups;
+}
+/**
  * set ASI field value
- * @param fieldName the asi field name
- * @param value the value to set
+ * 
+ * @param fieldName
+ *            the asi field name
+ * @param value
+ *            the value to set
  */
 ACABASE.prototype.setFieldValue = function(fieldName, value) {
 
@@ -99,24 +181,30 @@ ACABASE.prototype.setFieldValue = function(fieldName, value) {
 
 					field.setChecklistComment(value);
 					group.setFields(fields);
+					break;
 				}
 			}
 		}
 	}
 	this.asiGroups = asiGroups;
+	return asiGroups;
 }
 
 /**
- * set ASI drop down field values ( this one is useful in case you have a dynamic dropdown list);
- * @param fieldName the drop down field name
- * @param objValues the object of values. ex: var obj={}. obj["key"]="value";
+ * set ASI drop down field values ( this one is useful in case you have a
+ * dynamic dropdown list);
+ * 
+ * @param fieldName
+ *            the drop down field name
+ * @param objValues
+ *            the object of values. ex: var obj={}. obj["key"]="value";
  */
 ACABASE.prototype.setDropdownValues = function(fieldName, objValues) {
 
 	var value = aa.util.newArrayList();
 	for ( var val in objValues) {
 		var model = aa.proxyInvoker.newInstance("com.accela.aa.aamain.cap.RefAppSpecInfoDropDownModel").getOutput();
-		model.setAttrValue(val);
+		model.setAttrValue(objValues[val]);
 		model.setResAttrValue(objValues[val]);
 		model.setFieldLabel(fieldName);
 		value.add(model);
@@ -142,13 +230,53 @@ ACABASE.prototype.setDropdownValues = function(fieldName, objValues) {
 	this.asiGroups = asiGroups;
 
 }
+/**
+ * Gets the ASI Tables on the record in a hashmap with the table name as the key
+ * and the table value array as the value
+ */
+ACABASE.prototype.getASITables = function() {
 
+	var tableMap = aa.util.newHashMap();
+	var gm = this.getAsitGroups();
+
+	var ta = gm.getTablesMap();
+	var tai = ta.values().iterator();
+	while (tai.hasNext()) {
+		var tsm = tai.next();
+
+		if (tsm.rowIndex.isEmpty())
+			continue;
+		var tempObject = new Array;
+		var tempArray = new Array;
+		var tn = tsm.getTableName();
+
+		var tsmfldi = tsm.getTableField().iterator();
+		var tsmcoli = tsm.getColumns().iterator();
+		var numrows = 1;
+		while (tsmfldi.hasNext()) {
+			if (!tsmcoli.hasNext()) {
+				var tsmcoli = tsm.getColumns().iterator();
+				tempArray.push(tempObject);
+				var tempObject = new Array;
+				numrows++
+			}
+			var tcol = tsmcoli.next();
+			var tval = tsmfldi.next();
+			tempObject[tcol.getColumnName()] = tval
+		}
+		tempArray.push(tempObject);
+		tableMap.put(tn, tempArray);
+	}
+
+	return tableMap;
+}
 /**
  * internal function to be executed after the execution
  */
 ACABASE.prototype.afterExecute = function() {
 	var capModel = this.getCapModel();
 	capModel.setAppSpecificInfoGroups(this.getAsiGroups());
+	capModel.setAppSpecificTableGroupModel(this.getAsitGroups())
 	aa.env.setValue("CapModel", capModel);
 
 	if (this.tdebug.indexOf("**ERROR") > 0) {
@@ -172,7 +300,8 @@ ACABASE.prototype.afterExecute = function() {
 }
 
 /**
- * internal function, should be called after the implementation in the page flow script
+ * internal function, should be called after the implementation in the page flow
+ * script
  */
 function run() {
 	try {
