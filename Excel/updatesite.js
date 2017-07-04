3e52a4502cbdc86a367ce7d6ce31aba30cb11fac
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-04 15:17:44 +0400

    1.2.5

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,5 +1,5 @@
 [ {
-	VERSION : "1.2.4.0",
-	RELEASENOTES : [ "Notifications is now included", "Fix for workflow validation to handle space on flow control" ],
+	VERSION : "1.2.5.0",
+	RELEASENOTES : [ "Notifications is now included", "Fix for error messages in case of huge count","Fix for workflow validation to handle space on flow control" ],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
