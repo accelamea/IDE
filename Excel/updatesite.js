036463bd307a6fe58ac885fb6a720840ad659851
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-12 15:08:19 +0400

    1.2.8 excel

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,5 +1,8 @@
 [ {
-	VERSION : "1.2.7.0",
-	RELEASENOTES : [ "Stop Uploading/Downloading Default Sequences", "Alias is now mandatory in record Type","Fix for workflow validation on non used tasks" ],
+	VERSION : "1.2.8.0",
+	RELEASENOTES : [ "Stop Uploading/Downloading Default Sequences",
+	                 "Alias is now mandatory in record Type",
+	                 "Fix for workflow validation on non used tasks",
+	                 "Minor bug fixing"],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
