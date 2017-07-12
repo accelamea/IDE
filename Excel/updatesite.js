1f3daef39f9ab31fb751695ba15d4b01929f4239
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-12 13:53:06 +0400

    1.2.6 excel plugin

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,5 +1,5 @@
 [ {
-	VERSION : "1.2.5.0",
-	RELEASENOTES : [ "Notifications is now included", "Fix for error messages in case of huge count","Fix for workflow validation to handle space on flow control" ],
+	VERSION : "1.2.6.0",
+	RELEASENOTES : [ "Stop Uploading/Downloading Default Sequences", "Alias is now mandatory in record Type","Fix for workflow validation on non used tasks" ],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
