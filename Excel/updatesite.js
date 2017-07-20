5e0349e4368b92ba949e7017b82cf79890b5e9c6
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-20 09:24:19 +0400

    1.3.3

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,7 +1,10 @@
 [ {
-	VERSION : "1.3.2.0",
-	RELEASENOTES : [ "Fixing workflow validation bug on first task if ID is not used",
-	                 "Adding new sheet button, working with template version 1.3.2 and above",
-	                 "Adding the possibility to fill a word template from the workbook(beta version)"],
+	VERSION : "1.3.3.0",
+	RELEASENOTES : [ "Adding new sheet button, working with template version 1.3.2 and above",
+	                 "Adding the possibility to fill a word template from the workbook",
+	                 "Adding the possibility to load sheet settings from file",
+	                 "Interface redesign for best practice",
+	                 "Now the plugin will be visible only for Accela Template",
+	                 "Performance enahcements"],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
