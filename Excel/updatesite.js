e8a3f23188663a436c370d55fbc4ce5c82a3a89d
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-25 13:15:06 +0400

    1.3.7

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,8 @@
 [ {
-	VERSION : "1.3.6.0",
-	RELEASENOTES : [ "UI enhancements",
+	VERSION : "1.3.7.0",
+	RELEASENOTES : ["Smartchoice mapping fix",
+	                "Performance Enhancements",
+	                "UI enhancements",
 	                 "Word bookmark enchancement",
 	                 "Gateway SSL bug fix",
 	                 "DDL download on multi-agency bug fix"],
