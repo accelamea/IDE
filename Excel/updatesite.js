ed346610d796046329e23cf5d84267897422d80a
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-07-30 10:35:58 +0400

    1.4.1

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,9 @@
 [ {
-	VERSION : "1.4.0.0",
-	RELEASENOTES : ["Duplication validation across all sheets",
+	VERSION : "1.4.1.0",
+	RELEASENOTES : ["Workflow empty success message fix",
+	                "ASI/ASIT default value download fix",
+	                "Workflow reference space validation issue fix",
+	                "Duplication validation across all sheets",
 	                "Performance Enhancements",
 	                "Download wont override non accela fields"],
 	PATH : "AccelaExcelPlugin.msi"
