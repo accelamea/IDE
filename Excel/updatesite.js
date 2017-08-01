095e34cf98feefdaead79da58e4e8efcca5ae367
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-08-01 17:24:39 +0400

    1.4.3

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,12 +1,13 @@
 [ {
-	VERSION : "1.4.2.0",
-	RELEASENOTES : ["Lock feature to avoid changing configurations by multiple users in the same time",
+	VERSION : "1.4.3.0",
+	RELEASENOTES : [  "Performance Enhancements",
+	                  "Lock feature to avoid changing configurations by multiple users in the same time",
 	                "Minor interface Bugs fix",
 	                "Workflow empty success message fix",
 	                "ASI/ASIT default value download fix",
 	                "Workflow reference space validation issue fix",
 	                "Duplication validation across all sheets",
-	                "Performance Enhancements",
+	              
 	                "Download wont override non accela fields"],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
