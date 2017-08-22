47e2855410ed23dbe1490a053e74eec403102339
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-08-22 10:35:51 +0400

    1.4.6

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,7 @@
 [ {
-	VERSION : "1.4.5.0",
-	RELEASENOTES : [ "1.4.5: Adjust max length for DDL in ASI/ASIT",
+	VERSION : "1.4.6.0",
+	RELEASENOTES : [ "1.4.6: Fixing issue in mapping workflow with departments",
+	                 "1.4.5: Adjust max length for DDL in ASI/ASIT",
 	                 "1.4.4: Duplication validation for arabic labels in ASI/ASIT/WORKFLOW",
 	                 "1.4.4: Require DDL if the type is Shared Dropdown List in ASI/ASIT",
 	                 "1.4.4: Require DEFAULT_VALUE if the type is Shared Dropdown List and the field is required in ASI/ASIT",
