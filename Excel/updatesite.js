a9355c5f64bc4b16c6bc2e45ba76d009380a2219
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-09-07 14:25:06 +0400

    1.4.7

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,11 +1,8 @@
 [ {
-	VERSION : "1.4.6.0",
-	RELEASENOTES : [ "1.4.6: Fixing issue in mapping workflow with departments",
-	                 "1.4.5: Adjust max length for DDL in ASI/ASIT",
-	                 "1.4.4: Duplication validation for arabic labels in ASI/ASIT/WORKFLOW",
-	                 "1.4.4: Require DDL if the type is Shared Dropdown List in ASI/ASIT",
-	                 "1.4.4: Require DEFAULT_VALUE if the type is Shared Dropdown List and the field is required in ASI/ASIT",
-	                 "1.4.4: Fix for drill down issue(DB package)",
-	                 "1.4.4: Fix for Filter Check on Record Type Update(DB package)"],
+	VERSION : "1.4.7.0",
+	RELEASENOTES : [ "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
+	                 "1.4.6: Fixing issue in mapping workflow with departments",
+	                 "1.4.5: Adjust max length for DDL in ASI/ASIT"
+	               ],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
