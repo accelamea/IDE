b1abed9b8e5faa53d49d1f081250127383700a8a
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-09-13 11:06:01 +0400

    1.4.8

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,8 +1,8 @@
 [ {
-	VERSION : "1.4.7.0",
-	RELEASENOTES : [ "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
-	                 "1.4.6: Fixing issue in mapping workflow with departments",
-	                 "1.4.5: Adjust max length for DDL in ASI/ASIT"
+	VERSION : "1.4.8.0",
+	RELEASENOTES : [ "1.4.8: Fixing shared Dropdow Download Issues",
+	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
+	                 "1.4.6: Fixing issue in mapping workflow with departments"	                 
 	               ],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
