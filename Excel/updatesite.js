d6a20ef1e46b888a255811ad787e6654e18f5a87
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-10-04 15:56:29 +0400

    1.4.9.0

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,7 @@
 [ {
-	VERSION : "1.4.8.0",
-	RELEASENOTES : [ "1.4.8: Fixing shared Dropdow Download Issues",
+	VERSION : "1.4.9.0",
+	RELEASENOTES : [ "1.4.9: Fixing export to Word Template issue",
+	                 "1.4.8: Fixing shared Dropdow Download Issues",
 	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
 	                 "1.4.6: Fixing issue in mapping workflow with departments"	                 
 	               ],
