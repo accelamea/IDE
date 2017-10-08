5df2f3fac3e6702a966860e8d605e9e91899d0f7
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-10-08 11:51:12 +0400

    Excel Plugin: 1.5.0

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,7 @@
 [ {
-	VERSION : "1.4.9.0",
-	RELEASENOTES : [ "1.4.9: Fixing export to Word Template issue",
+	VERSION : "1.5.0.0",
+	RELEASENOTES : [ "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
+	                 "1.4.9: Fixing export to Word Template issue",
 	                 "1.4.8: Fixing shared Dropdow Download Issues",
 	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
 	                 "1.4.6: Fixing issue in mapping workflow with departments"	                 
