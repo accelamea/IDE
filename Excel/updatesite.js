909a973c1c82170704927845a16e74206dcee815
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-10-24 16:49:04 +0400

    fixing comment

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,6 @@
 [ {
 	VERSION : "1.5.3.0",
-	RELEASENOTES : [ "1.5.2: Fixing worflow sheet issue", 
+	RELEASENOTES : [ "1.5.3: Fixing worflow sheet issue", 
 	                 "1.5.2: Fixing Missing Value(H) for display in ACA in ASI and ASIT",  
 	                 "1.5.1: Fixing issue inspection sheet validation"	,            
 	                 "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
