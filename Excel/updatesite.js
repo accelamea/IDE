9333a42474ad07073ebd63f5bf9162e6b7429018
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-10-24 10:02:25 +0400

    1.5.1

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,10 +1,11 @@
 [ {
-	VERSION : "1.5.0.0",
-	RELEASENOTES : [ "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
+	VERSION : "1.5.1.0",
+	RELEASENOTES : [ "1.5.1: Fixing issue inspection sheet validation"	,            
+	                 "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
 	                 "1.4.9: Fixing export to Word Template issue",
 	                 "1.4.8: Fixing shared Dropdow Download Issues",
-	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT",
-	                 "1.4.6: Fixing issue in mapping workflow with departments"	                 
+	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT"
+	                      
 	               ],
 	PATH : "AccelaExcelPlugin.msi"
 } ]
