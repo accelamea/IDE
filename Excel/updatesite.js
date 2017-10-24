5975511fca4ed4e151502df969ed3a8b5fa9a363
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-10-24 14:25:43 +0400

    1.5.2

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,7 @@
 [ {
-	VERSION : "1.5.1.0",
-	RELEASENOTES : [ "1.5.1: Fixing issue inspection sheet validation"	,            
+	VERSION : "1.5.2.0",
+	RELEASENOTES : [ "1.5.2: Fixing Missing Value(H) for display in ACA in ASI and ASIT",  
+	                 "1.5.1: Fixing issue inspection sheet validation"	,            
 	                 "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
 	                 "1.4.9: Fixing export to Word Template issue",
 	                 "1.4.8: Fixing shared Dropdow Download Issues",
