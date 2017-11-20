baca83d29a03805e61bd353ea794becd4e736edc
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2017-11-20 16:12:16 +0400

    fixing bugs

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,15 +1,15 @@
 [ {
-	VERSION : "1.5.7.0",
-	RELEASENOTES : [ "1.5.7: Enhanced Login and improvements on Word Export feature", 
+	VERSION : "1.5.8.0",
+	RELEASENOTES : [ "1.5.8: Fixing bugs in Word Export feature",
+	                 "1.5.7: Fixing bugs in Word Export feature",
+	                 "1.5.6: Enhanced Login and improvements on Word Export feature", 
 	                 "1.5.5: Enhanced error for department duplicated name", 
 	                 "1.5.4: TSI data type validation, field is mandatory now", 
 	                 "1.5.3: Fixing worflow sheet issue", 
 	                 "1.5.2: Fixing Missing Value(H) for display in ACA in ASI and ASIT",  
 	                 "1.5.1: Fixing issue inspection sheet validation"	,            
-	                 "1.5.0: Including Field Alias for ASI sheet (Require Database Update)",
-	                 "1.4.9: Fixing export to Word Template issue",
-	                 "1.4.8: Fixing shared Dropdow Download Issues",
-	                 "1.4.7: DEFAULT_VALUE is now optional for ASIT and ASIT"
+	                 "1.5.0: Including Field Alias for ASI sheet (Require Database Update)"
+	               
 	                      
 	               ],
 	PATH : "AccelaExcelPlugin.msi"
