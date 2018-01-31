d75ed44504a21ca4f25124cb02d3fed3bf140512
Sleiman KOZAIZAN <slymen_k@hotmail.com>
2018-01-31 11:06:08 +0400

    2.0.1.0

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,7 @@
 [ {
-	VERSION : "2.0.0.0",
-	RELEASENOTES : [ "2.0.0: Major release, including bug fixing and Create Swimlanes feature",
+	VERSION : "2.0.1.0",
+	RELEASENOTES : [ "2.0.1: upload button is always disabled bug fix",
+	                 "2.0.0: Major release, including bug fixing and Create Swimlanes feature",
 	                 "1.5.8: Fixing bugs in Word Export feature",
 	                 "1.5.7: Fixing bugs in Word Export feature",
 	                 "1.5.6: Enhanced Login and improvements on Word Export feature", 
