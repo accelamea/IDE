5af085e9ea8669ec7a29cb8901c8f091fe4b8c56
Yaseen Altiti <ytiti@accela.com>
2019-03-06 17:44:34 +0400

    Release 2.0.7

--- Excel/updatesite.js
+++ Excel/updatesite.js
@@ -1,6 +1,8 @@
 [ {
-	VERSION : "2.0.5.0",
-	RELEASENOTES : [ "2.0.5: General fixes and parallel tasks generation",
+	VERSION : "2.0.7.0",
+	RELEASENOTES : [ 
+					"2.0.7: Generate test cases enhancements",
+					"2.0.5: General fixes and parallel tasks generation",
 					 "2.0.4: Generate test cases",
 					 "2.0.3: Fixing minor issues",
 					 "2.0.2: upload button is always disabled bug fix",
